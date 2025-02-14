224b737eafb78a68996a97c10800e9818fc81510
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-14 08:40:52 +0200

    feat: installing django-filters and implement CharacterListView

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from characters.views import get_random_character_view
+from characters.views import get_random_character_view, CharacterListView
 
 urlpatterns = [
-    path("characters/random/", get_random_character_view, name="character-random"),
+    path(
+        "characters/random/",
+        get_random_character_view,
+        name="character-random"
+    ),
+    path("characters/", CharacterListView.as_view(), name="character-list"),
 ]
 
 app_name = "characters"
