7102cd26757fc98fff36aa61709d0a0f26f146d6
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-14 22:03:42 +0200

    feat: add celery with django-celery-beat schedular

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "drf_spectacular",
+    "django_celery_beat",
     "characters",
 ]
 
@@ -132,3 +133,9 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Kyiv"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
