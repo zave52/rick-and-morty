428f9eb9209357e09fef7d809dc91a0219f016a6
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-14 09:08:28 +0200

    feat: add swagger documentation using drf-spectacular

--- characters/views.py
+++ characters/views.py
@@ -2,6 +2,8 @@ import random
 
 from django_filters.rest_framework import DjangoFilterBackend
 from django.http import HttpRequest, HttpResponse
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,8 +12,12 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(
+    responses={status.HTTP_200_OK, CharacterSerializer}
+)
 @api_view(["GET"])
 def get_random_character_view(request: HttpRequest) -> HttpResponse:
+    """Get random character from Rick & Morty world."""
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
@@ -24,5 +30,19 @@ class CharacterListView(generics.ListAPIView):
     serializer_class = CharacterSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = {
-        "name": ["exact", "icontains"]
+        "name": ["icontains"]
     }
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name__icontains",
+                type=OpenApiTypes.STR,
+                description="Filter by name insensitive contains",
+                required=False,
+            )
+        ]
+    )
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """List characters with filter by name."""
+        return super().get(request, *args, **kwargs)
