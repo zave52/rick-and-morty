082172de998c3d819be99cf2ac0232710560673c
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-15 09:48:03 +0200

    feat: add default state for django debug

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() in ("true", "t", "1")
+DEBUG = str(os.environ.get("DJANGO_DEBUG", False)).lower() in ("true", "t", "1")
 
 ALLOWED_HOSTS = []
 
